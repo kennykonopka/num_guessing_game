b4f1d8243d6036f645ac87e1dfcc2b1125ebd26d
Kenny Konopka <kmk7565@psu.edu>
2026-04-27 17:34:57 -0400

    init and set_diff

--- num_guess.py
+++ num_guess.py
@@ -1,1 +1,19 @@
+#Number Guessing Game
+#Features num guessing game with levels ranging from easy, to medium, to hard
 
+
+
+
+class Game():
+
+    def init(self, guesses = 10, diff = "easy"):
+        self.guesses = guesses
+        self.diff = self.set_difficulty(diff)
+
+    
+    def set_difficulty(self, diff):
+        diff = diff.lower()
+
+        diff_levels = {"easy": 100, "medium": 300, "hard" : 1000}
+
+        return diff_levels[diff]
